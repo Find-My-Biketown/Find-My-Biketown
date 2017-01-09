383985993d698a36e13ae2efba7797f0e3252df2
cmd-kvn <wong.kevin@student.clackamas.edu>
2017-01-09 15:54:24 -0800

    added algorithm to return sorted list of nearby bike stations

---
+++ scripts/models/outputModel.js
@@ -0,0 +1,17 @@
+// Access the validated user’s address object from the input form
+
+// Call the Biketown API to grab station info, specifically lat/long coordinates. Other possibilities: name, number of bikes available.
+// And possibly store it, an array of objects, in local storage?
+
+// Create a function to calculate the distance between the user’s address and each Biketown station
+// The function takes user’s address as one argument and an array of objects with the bike station coordinates as the second argument
+
+// Copy the bike station array of objects with .map()? and add a distance property
+
+// Use the distance formula d=√​(x​2​​−x​1​​)​2​​+(y​2​​−y​1​​)​2​​​​​ to give each distance property a value
+
+// .sort(compare(a,b) the mapped? array by the distance property, ascending order
+
+// Return the array of objects
+
+// Append the returned array of objects from the distance function (up to a determined amount i.e. 5 indexes) as a list to the view/HTML
