7c13dcdb7079a9d94b862fe3cb225911153270ab
meghanroserebecca <meghanroserebecca@gmail.com>
2017-01-10 15:46:06 -0800

    deleted old useless code

--- scripts/models/userObjectModel.js
+++ scripts/models/userObjectModel.js
@@ -1,25 +1,3 @@
-// $.fn.serializeObject = function() {
-//   var userObject = {};
-//   var serialized = this.serializeArray();
-//   $.each(serialized, function() {
-//     if (userObject[this.name] !== undefined) {
-//       if (!userObject[this.name].push) {
-//         userObject[this.name] = [userObject[this.name]];
-//       }
-//       userObject[this.name].push(this.value || '');
-//     } else {
-//       userObject[this.name] = this.value || '';
-//     }
-//   });
-//   return userObject;
-// };
-//
-// $(function() {
-//   $('form').submit(function() {
-//     $('#result').text(JSON.stringify($('form').serializeObject()));
-//     return false;
-//   });
-// });
 $.fn.serializeJSON=function() {
   var json = {};
   jQuery.map($(this).serializeArray(), function(n, i){
