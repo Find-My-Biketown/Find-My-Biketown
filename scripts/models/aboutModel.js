8d5012499f82a38d52cba97461fd233bfc375fbc
cmd-kvn <wong.kevin@student.clackamas.edu>
2017-01-11 15:27:27 -0800

    added script tags in index for new files continuing work on handlebars for bios

---
+++ scripts/models/aboutModel.js
@@ -0,0 +1,7 @@
+(function(module) {
+  var about = {};
+
+
+
+  module.about = about;
+})(window);
--- scripts/views/aboutView.js
+++ scripts/views/aboutView.js
@@ -1,7 +1,15 @@
 (function(module) {
   var aboutView = {};
 
+  aboutView.toHtml = function (context){
+    var source = $('#bio-template').html(); // grab the template
 
+    var template = Handlebars.compile(source); // compile the template
+    var compile = template(context); // pass data to the template
+
+    // move this to view/controller?
+    $('.dev-bio-articles').html(compile); // add the compiled html to the page
+  };
 
   module.aboutView = aboutView;
 })(window);
