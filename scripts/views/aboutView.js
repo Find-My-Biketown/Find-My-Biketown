9c02e87c2536da93977cb65554ef0cd953b38898
cmd-kvn <wong.kevin@student.clackamas.edu>
2017-01-11 18:20:03 -0800

    created successful ajax call to set devBios to localStorage

--- scripts/views/aboutView.js
+++ scripts/views/aboutView.js
@@ -11,5 +11,21 @@
     $('.dev-bio-articles').html(compile); // add the compiled html to the page
   };
 
+// this will probably be moved eventually
+  //aboutView.loadAboutPage = function (callback){
+  $.ajax('/data/devBios.json',{
+    method: 'GET',
+    success: function(response){
+      localStorage.setItem('devBios', JSON.stringify(response));
+      console.log('response is: ', response);
+    },
+    error: function(response){
+      alert('error ', response);
+    }
+  });//end ajax
+  //};//end aboutView.loadAboutPage
+// So the ajax call gets the data locally, or from the app source, then sets it to localStorage, then will be able to be pulled down to render the about bios
+
+  //aboutView.loadAboutPage();
   module.aboutView = aboutView;
 })(window);
