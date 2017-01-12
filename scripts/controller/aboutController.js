67790b9468b331000496b7710321b3d5a8dfdff5
cmd-kvn <wong.kevin@student.clackamas.edu>
2017-01-12 14:27:16 -0800

    completed functionality to show only 'dev team' link

--- scripts/controller/aboutController.js
+++ scripts/controller/aboutController.js
@@ -1,7 +1,7 @@
 (function(module) {
   var aboutController = {};
 
-  welcomeController.reveal = function() {
+  aboutController.reveal = function() {
     // use jquery to grab and hide all the first children of parent main in the main html, filter to select #dev-bio-page, and show it
     $('main > *').hide().filter('#dev-bio-page').show();
   };
